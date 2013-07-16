1b699677b1c097ecaca15b22ddf3abf03b2b0164
ericsims(laptop) <EricSimsM@gmail.com>
2013-07-16 10:54:29 -0400

    added ar-drone calibrate.js

---
+++ ar-drone calibrate.js
@@ -0,0 +1,17 @@
+var arDrone = require('ar-drone');
+var client = arDrone.createClient({ip: '192.168.1.10'});
+
+client.config('control:altitude_max', 500);
+
+client.takeoff();
+client
+.after(5000, function() {
+	this.calibrate(0);
+})
+.after(10000, function() {
+	this.stop();
+	this.land();
+})
+.after(1000, function() {
+	progess.exit(1);
+});
