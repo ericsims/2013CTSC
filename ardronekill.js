447acba41b2be4bdba5677cb42ef461cd3e4aa9a
ericsims(laptop) <EricSimsM@gmail.com>
2013-07-18 10:51:25 -0400

    added ardronekill.js halts drone through telnet

---
+++ ardronekill.js
@@ -0,0 +1,14 @@
+var net = require('net');
+var client = net.connect(23, '192.168.1.10', function(){
+   client.on('data', function(data) {
+     console.log('data:', data.toString());
+   });
+
+   client.on('error', function(err) {
+     console.log('error:', err.message);
+   });
+
+   client.write('halt\n');
+   client.write('exit\n');
+   process.exit();
+});
