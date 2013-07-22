e594d37733be37abd25fc5a20fdfc24805589c9b
Eric Sims <EricSimsM@gmail.com>
2013-07-21 23:24:06 -0400

    added onboard node.js files

---
+++ onboard/testSend.js
@@ -0,0 +1,13 @@
+var serialport = require("serialport").SerialPort;
+var sys = require("sys");
+
+console.log('test')
+
+var sp = new serialport.SerialPort("/dev/ttyO3", {
+	baud: 9600
+});
+
+setInterval(function (){
+	serial_port.write("B");
+
+}, 1000);
