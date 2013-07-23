988f155b3f4a58cde2ce55838434731df867dccb
ericsims(laptop) <EricSimsM@gmail.com>
2013-07-23 13:18:06 -0400

    deleted old files

--- navdataParse.js
+++
@@ -1,3 +0,0 @@
-var data = function(data){
-	return this.data;
-};
--- vision/BGR to HSV.js
+++
@@ -1,5 +0,0 @@
-var cv = require('opencv');
-cv.readImage('./matrix.png', function(err, im_orig) {
-	var big = cv.cvtColor(im_orig, cv.CV_BGR2HSV);
-	big.save('./big.png');
-});
