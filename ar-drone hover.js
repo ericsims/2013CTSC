12dd894948285b9364164999e8b11ec585615f64
ericsims(laptop) <EricSimsM@gmail.com>
2013-07-25 18:45:20 -0400

    started flat trim calibration

--- ar-drone hover.js
+++ ar-drone hover.js
@@ -1,12 +1,17 @@
 var arDrone = require('ar-drone');
-var client = arDrone.createClient({ip: '192.168.1.11'});
+var client = arDrone.createClient({ip: '192.168.1.10'});
 
 client.config('control:altitude_max', 1000);
 
 client.on('navdata', console.log);
 
-client.takeoff();
+client.config('control:ardrone_at_set_flat_trim', 1);
 client
+.after(10000, function() {
+
+	process.exit();
+	//client.takeoff();
+})
 .after(90000, function() {
 	this.stop();
 	this.land();
